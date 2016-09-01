03829e670552d1ad54c0ac2376d5c39ea561dd7e
abhishek_kr <abhishekk@inferess.com>
2016-08-31 22:33:17 -0400

    Update pom for jdbc driver

--- src/main/java/com/ak/SlackScheduler.java
+++ src/main/java/com/ak/SlackScheduler.java
@@ -1,7 +1,17 @@
 package com.ak;
 
+import java.util.Timer;
+
 /**
  * Created by kumar on 8/31/16.
  */
 public class SlackScheduler {
+
+    public static void main(String ...args) throws Exception {
+
+        Timer timer = new Timer();
+        ScheduledTask st = new ScheduledTask();
+        timer.schedule(st, 0, 60000);
+    }
+
 }
